b2df6fe24b93cf2605195e1158abb49a0a0689b2
Day HONG <tot032874@gmail.com>
2025-04-12 21:14:10 +0900

    feat/landing #9

---
+++ app/src/main/java/com/data/app/presentation/login/LandingActivity.kt
@@ -0,0 +1,43 @@
+package com.data.app.presentation.login
+
+import android.content.Intent
+import android.graphics.Typeface
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.text.SpannableStringBuilder
+import android.text.Spanned
+import android.text.style.StyleSpan
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.data.app.R
+import com.data.app.databinding.ActivityLandingBinding
+
+class LandingActivity:AppCompatActivity() {
+
+    private lateinit var binding:ActivityLandingBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding=ActivityLandingBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val textView = findViewById<TextView>(R.id.tv_landing1)
+        val textData: String = textView.text.toString()
+        val builder = SpannableStringBuilder(textData)
+        val boldSpan = StyleSpan(Typeface.BOLD)
+        builder.setSpan(boldSpan, 6, 13, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+        builder.setSpan(boldSpan, 22, textData.length, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+        textView.text = builder
+
+        /*
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, LogInActivity::class.java)
+            startActivity(intent)
+            overridePendingTransition(android.R.anim.fade_in,android.R.anim.fade_out)
+        }, 2000)
+         */
+
+    }
+
+}
