3ab3f9e312302dda1f73da84628440b40875e069
Day HONG <tot032874@gmail.com>
2025-04-20 02:18:49 +0900

    feat/signup #4

---
+++ app/src/main/java/com/data/app/presentation/login/SignupViewmodel.kt
@@ -0,0 +1,10 @@
+package com.data.app.presentation.login
+
+import androidx.lifecycle.ViewModel
+
+class SignupViewmodel: ViewModel() {
+    lateinit var email: String
+    lateinit var username: String
+    lateinit var nationality: String
+    lateinit var password: String
+}
