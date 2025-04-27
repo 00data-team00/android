8d4224e3000c118245377a0dfb9ce96ad49e6035
tn-als <tnals@kyonggi.ac.kr>
2025-04-28 00:50:23 +0900

    feat/game-main #29

--- app/src/main/java/com/data/app/presentation/home/game/GameTabWeekAdapter.kt
+++ app/src/main/java/com/data/app/presentation/home/game/GameTabWeekAdapter.kt
@@ -2,7 +2,9 @@ package com.data.app.presentation.home.game
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
+import com.data.app.R
 import com.data.app.data.Week
 import com.data.app.databinding.ItemGameWeekBinding
 
@@ -29,6 +31,12 @@ class GameTabWeekAdapter:RecyclerView.Adapter<GameTabWeekAdapter.GameTabWeekView
         fun bind(week:Week){
             binding.tvDay.text=week.day
             binding.ivFlag.isSelected=week.flag
+
+            if (week.flag) {
+                binding.tvDay.setTextColor(ContextCompat.getColor(binding.root.context, R.color.game_flag_red))
+            } else {
+                binding.tvDay.setTextColor(ContextCompat.getColor(binding.root.context, R.color.game_flag_black))
+            }
         }
     }
 }
