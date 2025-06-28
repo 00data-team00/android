aaa1c2ea467e3162b02767c3897cb464d6087cb9
Day HONG <tot032874@gmail.com>
2025-06-28 16:55:38 +0900

    feat/game-quiz-final-success-or-fail #32 : linking not all done yet

--- app/src/main/java/com/data/app/presentation/main/home/game/quiz/GameQuizActivity.kt
+++ app/src/main/java/com/data/app/presentation/main/home/game/quiz/GameQuizActivity.kt
@@ -1,18 +1,21 @@
 package com.data.app.presentation.main.home.game.quiz
 
 import android.animation.ObjectAnimator
+import android.graphics.Color
 import android.os.Bundle
 import android.view.View
 import android.view.ViewGroup
 import androidx.activity.addCallback
 import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.updateLayoutParams
 import com.data.app.R
 import com.data.app.databinding.ActivityGameQuizBinding
+import android.graphics.PorterDuff
 import eightbitlab.com.blurview.BlurView
 import eightbitlab.com.blurview.RenderScriptBlur
 
@@ -75,6 +78,31 @@ class GameQuizActivity:AppCompatActivity() {
     fun updateProgress(current: Int, total: Int) {
         val percent = (current * 100) / total
         ObjectAnimator.ofInt(binding.progressQuiz, "progress", percent).setDuration(300).start()
+    }
+
+    private fun finalResult(success: Boolean){
+        if (success) {
+            binding.clGameQuiz.setBackgroundColor(Color.WHITE)
+            binding.tvGameSuccessfail.text = getString(R.string.game_quiz_finalsucess)
+            binding.tvGameComment.text = getString(R.string.game_quiz_commentsuccess)
+            binding.ivGameFinal.setImageResource(R.drawable.ic_correct)
+            binding.btnQuizNext.isSelected = true
+            binding.btnQuizAgain.isSelected = true
+            binding.rvWeeks.setBackgroundColor(ContextCompat.getColor(this, R.color.game_word_bg_green))
+        }
+        else{
+            binding.clGameQuiz.setBackgroundColor(ContextCompat.getColor(this, R.color.game_word_bg_green))
+            binding.tvGameSuccessfail.text = getString(R.string.game_quiz_finalfail)
+            binding.tvGameComment.text = getString(R.string.game_quiz_commentafail)
+            binding.ivGameFinal.setImageResource(R.drawable.ic_wrong)
+            binding.btnQuizNext.isSelected = false
+            binding.btnQuizAgain.isSelected = true
+            binding.rvWeeks.setBackgroundColor(Color.WHITE)
+            binding.progressQuiz.progressDrawable.setColorFilter(
+                ContextCompat.getColor(this, R.color.game_flag_red),
+                PorterDuff.Mode.SRC_IN)
+        }
+        binding.clGameSuccess.visibility = View.VISIBLE
     }
 
     fun barToFront(){
